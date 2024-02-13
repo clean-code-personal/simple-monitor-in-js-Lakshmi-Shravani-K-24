e83e0b15c82d660d28ac58d017ca63230fb8496b
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-13 19:51:49 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,41 +1,30 @@
-const {expect} = require('chai');
-
 const {TemperatureRange, SOCRange, ChargeRateRange}=require("./inputParameterRanges.js")
-function checkBreachType(value, min, max) {
+//checks whether inputParameters are in range or not and specifies the type of breach that is low or high
+function checkBreach(inputParameter, value, min, max) {
     if (value < min) {
-        return "low";
+        console.log(`!!! ${inputParameter} is low that is ${value} - Not in range!!!`);
+        return false;
     } else if (value > max) {
-        return "high";
+        console.log(`!!! ${inputParameter} is high that is ${value} - Not in range!!!`);
+        return false;
     }
-    return "nobreach";
+        console.log( `${inputParameter} is within the valid range that is : ${value}`) ;
+        return true;    
 }
-function printBreachType(inputParameter,value, breachType){
-    if(breachType!="nobreach"){
-    console.log(`!!! ${inputParameter} is ${breachType} that is : ${value} - Not in range!!!`);
+
+function batteryIsOk(temperature, soc, chargeRate){
+    const temperatureResult=checkBreach( "Temperature",temperature,TemperatureRange.min,TemperatureRange.max);
+    const socResult=checkBreach("soc", soc, SOCRange.min, SOCRange.max);
+    const chargeRateResult=checkBreach("chargeRate", chargeRate, ChargeRateRange.min, ChargeRateRange.max);
+    
+    if(temperatureResult & socResult & chargeRateResult ){
+        console.log("***Battery is Ok***");
+        return true;
+    }
+    else{
+    console.log("!!!Battery is not Ok!!!");
     return false;
     }
-else{
-console.log( `${inputParameter} is within the valid range that is : ${value}`) ;
-return true;}
-}
-
-function isBreached(inputParameter, value, min, max){
-    const breachType=checkBreachType(value,min, max);
-    const result=printBreachType( inputParameter,value,  breachType );
-return result;
-}
-function batteryIsOk(temperature, soc, chargeRate){
-const temperatureResult=isBreached( "Temperature",temperature,TemperatureRange.min,TemperatureRange.max);
-const socResult=isBreached("soc", soc, SOCRange.min, SOCRange.max);
-const chargeRateResult=isBreached("chargeRate", chargeRate, ChargeRateRange.min, ChargeRateRange.max);
-if(temperatureResult & socResult & chargeRateResult ){
-console.log("***Battery is Ok***");
-return true;
-}
-else{
-console.log("!!!Battery is not Ok!!!");
-return false;
-}
 }
 expect(batteryIsOk(25, 70, 0.7)).to.be.true;
 expect(batteryIsOk(50, 85, 0)).to.be.false;
