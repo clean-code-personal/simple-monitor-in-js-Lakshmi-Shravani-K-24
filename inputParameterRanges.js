363c179c68dfe185c54962800091fa6c4cffa003
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-13 18:42:17 +0530

    Create inputParameterRanges.js

---
+++ inputParameterRanges.js
@@ -0,0 +1,5 @@
+const  TemperatureRange= { min: 0, max: 45 }
+const SOCRange=  { min: 20, max: 80 }
+const  ChargeRateRange={min:0, max:0.8}
+
+module.exports ={TemperatureRange, SOCRange, ChargeRateRange};
