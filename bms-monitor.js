2a3def6c29f328ac99b7cae6e0cb0086fb0a8aa8
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-15 11:20:35 +0530

    Updated to print messages in two different languages

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,32 +1,60 @@
-const {TemperatureRange, SOCRange, ChargeRateRange}=require("./inputParameterRanges.js")
+const {
+  TemperatureRange,
+  SOCRange,
+  ChargeRateRange,
+  messageHashMap,
+} = require("./inputParameterRanges.js");
+const languageCode = 1; // 1: English / 2: German
 
 //checks whether inputParameters are in range or not  and returns boolean value and also specifies the type of breach that is low or high
 function isInRange(inputParameter, value, min, max) {
-    if (value < min) {
-        console.log(`!!! ${inputParameter} is low that is ${value} - Not in range!!!`);
-        return false;
-    } else if (value > max) {
-        console.log(`!!! ${inputParameter} is high that is ${value} - Not in range!!!`);
-        return false;
-    }
-        console.log( `${inputParameter} is within the valid range that is : ${value}`) ;
-        return true;    
+  if (value < min) {
+    return { input: inputParameter, code: 1, normal: false };
+  } else if (value > max) {
+    return { input: inputParameter, code: 2, normal: false };
+  }
+  return { input: inputParameter, code: 3, normal: true };
 }
 
-function batteryIsOk(temperature, soc, chargeRate){
-    const temperatureResult=isInRange( "Temperature",temperature,TemperatureRange.min,TemperatureRange.max);
-    const socResult=isInRange("soc", soc, SOCRange.min, SOCRange.max);
-    const chargeRateResult=isInRange("chargeRate", chargeRate, ChargeRateRange.min, ChargeRateRange.max);
-    const results=[temperatureResult, socResult,chargeRateResult ];
-    const finalResult = results.reduce((acc, result) => acc && result, true);
+function batteryIsOk(temperature, soc, chargeRate) {
+  //this function generates Message code and prints the message
+  function printMessage(result) {
+    const code = result.code; //code determines nputParameter's state low or high or normal
+    const inputParameter = result.input;
+    const messageCode = "" + languageCode + inputParameter + code; //message is combination of language , inputParameter and code
+    let message = messageHashMap.get(messageCode);
+    console.log(message);
+  }
+  const temperatureResult = isInRange(
+    "Temperature",
+    temperature,
+    TemperatureRange.min,
+    TemperatureRange.max
+  );
+  printMessage(temperatureResult);
+  const socResult = isInRange("SOC", soc, SOCRange.min, SOCRange.max);
+  printMessage(socResult);
+  const chargeRateResult = isInRange(
+    "ChargeRate",
+    chargeRate,
+    ChargeRateRange.min,
+    ChargeRateRange.max
+  );
+  printMessage(chargeRateResult);
 
-    if(finalResult){
-        console.log("***Battery is Ok***");
-        return true;
-    }
-    else{
-    console.log("!!!Battery is not Ok!!!");
+  const results = [
+    temperatureResult.normal,
+    socResult.normal,
+    chargeRateResult.normal,
+  ];
+
+  const finalResult = results.reduce((acc, result) => acc && result, true);
+  if (finalResult) {
+    printMessage({ input: "BatteryState", code: 4 });
+    return true;
+  } else {
+    printMessage({ input: "BatteryState", code: 5 });
     return false;
-    }
+  }
 }
-module.exports={batteryIsOk};
+module.exports = { batteryIsOk };
