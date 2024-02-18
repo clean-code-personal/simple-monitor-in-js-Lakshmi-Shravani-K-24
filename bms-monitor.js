8c65427da00c156b7188fcb000cfd64577810438
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-19 00:48:46 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,68 +1,69 @@
 const {
-  TemperatureRange,
-  SOCRange,
-  ChargeRateRange,
-  inputParameterMap,
-  messageHashMap,
-  languageCode,
-} = require("./inputParameterRanges.js");
-
-function isInRange(inputParameter, value, inputParameterRange) {
-  if (value < inputParameterRange.min) {
-    return {
-      input: inputParameter,
-      code: "low",
-      normal: false,
-    };
-  } else if (value > inputParameterRange.max) {
-    return {
-      input: inputParameter,
-      code: "high",
-      normal: false,
-    };
-  }
-
-  return {
-    input: inputParameter,
-    code: "normal",
-    normal: true,
-  };
-}
+    TemperatureRange,
+    SOCRange,
+    ChargeRateRange,
+    inputParameterMap,
+    messageHashMap,
+    languageCode,
+  } = require("./inputParameterRanges.js");
+const {isInRange, isInThresholdRange } = require("./rangeChecker.js");
+  
 function printMessage(result) {
-  const inputParameterCode = languageCode + result.input;
-  const messageCode = languageCode + result.code; //message is combination of language and code
-  const inputParameter = inputParameterMap.get(inputParameterCode); // get the parameter
-  let message = messageHashMap.get(messageCode); // get the message
-  console.log(inputParameter + message);
-}
-function batteryIsOk(temperature, soc, chargeRate) {
-  const temperatureResult = isInRange(
-    "Temperature",
-    temperature,
-    TemperatureRange
-  );
-  printMessage(temperatureResult);
-  //SOC
-  const socResult = isInRange("SOC", soc, SOCRange);
-  printMessage(socResult);
-  //  Charge Rate
-  const chargeRateResult = isInRange("ChargeRate", chargeRate, ChargeRateRange);
-  printMessage(chargeRateResult);
-
-  const results = [
-    temperatureResult.normal,
-    socResult.normal,
-    chargeRateResult.normal,
-  ];
-
-  const finalResult = results.reduce((acc, result) => acc && result, true);
-  if (finalResult) {
-    printMessage({ input: "Battery", code: "Ok" });
-    return true;
-  } else {
-    printMessage({ input: "Battery", code: "NotOk" });
-    return false;
+    const inputParameterCode = languageCode + result.input;
+    const messageCode = languageCode + result.code; //message is combination of language and code
+    const inputParameter = inputParameterMap.get(inputParameterCode); // get the parameter
+    let message = messageHashMap.get(messageCode); // get the message
+    console.log(inputParameter + message);
   }
-}
-batteryIsOk(48, 40, 0.9);
-module.exports = { batteryIsOk, printMessage };
+  function batteryIsOk(temperature, soc, chargeRate) {
+    const temperatureResult = isInRange(
+      "Temperature",
+      temperature,
+      TemperatureRange
+    );
+    //SOC
+    const socResult = isInRange("SOC", soc, SOCRange);
+    //  Charge Rate
+    const chargeRateResult = isInRange("ChargeRate", chargeRate, ChargeRateRange);
+    printMessage(temperatureResult);
+    printMessage(socResult);
+    printMessage(chargeRateResult);
+   
+    const results = [
+      temperatureResult.normal,
+      socResult.normal,
+      chargeRateResult.normal,
+    ];
+    const finalResult = results.reduce((acc, result) => acc && result, true);
+    if (finalResult) {
+      printMessage({ input: "Battery", code: "Ok" });
+      return true;
+    } else {
+      printMessage({ input: "Battery", code: "NotOk" });
+      return false;
+    }
+  }
+
+  function batteryWarningIndicator(temperature, soc, chargeRate){
+    const temperatureWarning=isInThresholdRange("Temperature",
+    temperature,
+    TemperatureRange);
+    const socWarning=isInThresholdRange("SOC",soc,SOCRange);
+    const chargeRateWarning= isInThresholdRange("ChargeRate", chargeRate, ChargeRateRange);
+
+    printMessage(temperatureWarning);
+    printMessage(socWarning);
+    printMessage(chargeRateWarning);
+
+    const warnings=[temperatureWarning.isSafe,socWarning.isSafe,chargeRateWarning.isSafe];
+    const finalWarningResult= warnings.reduce((acc, result) => acc && result, true);
+    if (finalWarningResult) {
+      printMessage({ input: "Battery", code: "Safe" });
+      return true;
+    } else {
+      printMessage({ input: "Battery", code: "NotSafe" });
+      return false;
+    }
+
+  }
+  module.exports = { batteryIsOk,batteryWarningIndicator };
